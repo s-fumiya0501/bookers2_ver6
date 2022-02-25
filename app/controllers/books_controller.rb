e4010f79d190993ade5184df98de6c65abddfbf5
s-fumiya0501 <19t3058n@vc.ibaraki.ac.jp>
2022-02-25 16:44:17 +0900

    [complete]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,53 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @new_book = Book.new
+    @user = @book.user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user_id!=current_user.id
+      redirect_to books_path
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id),notice: 'You have created book successfully.'
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book),notice:"You have updated book successfully."
+    else
+      render :edit
+    end
+  end
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
