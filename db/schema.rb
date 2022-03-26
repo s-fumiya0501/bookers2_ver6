67a9d3af7336c7ecf7adac5439e1bcd9ead2ee94
ls-contents <68625909+ls-contents@users.noreply.github.com>
2022-03-26 22:01:22 +0900

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_03_25_120528) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
